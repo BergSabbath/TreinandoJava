6ab1f2ec34711baf327a2cd647c5dea83d26a8f2
BergSabbath <bergit7@gmail.com>
2020-10-19 14:39:12 -0300

    tabuada com for, while, e do-while

--- Tabuada/src/tabuada/Tabuada.java
+++ Tabuada/src/tabuada/Tabuada.java
@@ -8,18 +8,39 @@ public class Tabuada {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         
-    System.out.println("Digite um número para saber a sua tabuada de 1 a 10");
+        //utilizando While
+        System.out.println("(while)\n Digite um número para saber a sua tabuada de 1 a 10:");
         int num = entrada.nextInt();
-    int i = 1 ; 
-    
-    while(i <=10){
-       int Resul = i * num;
+        int i = 1 ; 
+        while(i <=10){
+        int Resul = num * i;
        
-    System.out.println(i + " x " +num+ " = "+Resul);
+        System.out.println(num + " x " +i+ " = "+Resul);
             
-       i++;
-   }
+        i++;
+        }
+        //utilizando do-while
+        int o =1;
+        System.out.println("(do-while)\n Digite outro numero: ");
+        int num2 = entrada.nextInt();
+        do{
+           int Resul2 = num2 * o;
+           
+           System.out.println(num2 + " x " + o +" = "+ Resul2);
+           o++;           
+        }while(o <= 10);
+        
+        //utlizando for
+        System.out.println("(for)\n Digite mais outro numero: ");
+        int num3 = entrada.nextInt();
+        for(int e = 1; e < 11; e++){
+            int Resul3 = num3 * e;
+          System.out.println(num3 +" x "+ e +" = "+ Resul3);
+        }
+        
     
     }
+    
+    
     
 }
